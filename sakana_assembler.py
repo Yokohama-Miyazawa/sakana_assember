0cefc9b9dbd9c1899864fb57279a03db74cbd942
宮澤 修 <m1411182@edu.cc.uec.ac.jp>
2019-07-16 21:58:44 +0900

    sakana_assembler.pyを作成．

---
+++ sakana_assembler.py
@@ -0,0 +1,63 @@
+import sys
+
+
+def asm_to_bin(asm):
+    inst = asm[0:3]
+    if inst == "NOP":
+        return "0000\n"
+    elif inst == "MOV":
+        arg0, arg1 = asm[4], asm[7]
+        if arg0 == "A" and arg1 == "0":
+            return "0100\n"
+        elif arg0 == "B" and arg1 == "0":
+            return "0101\n"
+        elif arg0 == "A" and arg1 == "B":
+            return "0110\n"
+        elif arg0 == "B" and arg1 == "A":
+            return "0111\n"
+        else:
+            raise Exception
+    elif inst == "ADD":
+        arg0, arg1 = asm[4], asm[7]
+        if arg0 == "A" and arg1 == "1":
+            return "1000\n"
+        elif arg0 == "A" and arg1 == "2":
+            return "1001\n"
+        elif arg0 == "A" and arg1 == "A":
+            return "1010\n"
+        elif arg0 == "A" and arg1 == "B":
+            return "1011\n"
+        else:
+            raise Exception
+    else:
+        raise Exception
+
+
+def assemble(input_file_name):
+    try:
+        f = open(input_file_name)
+        o = open("out.txt", "w", encoding="UTF-8")
+    except FileNotFoundError:
+        print("{}というファイルは存在しません．".format(input_file_name))
+    else:
+        try:
+            for line in f:
+                bin_code = asm_to_bin(line)
+                print(bin_code[:-1], end=' ')
+                print(line, end='')
+                o.write(bin_code)
+        finally:
+            f.close()
+            o.close()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    try:
+        input_file_name = args[1]
+    except IndexError:
+        print("入力ファイルが指定されていません．\n例: python sakana_assembler.py code.asm")
+    else:
+        print(input_file_name)
+        assemble(input_file_name)
